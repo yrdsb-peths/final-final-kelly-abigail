ce9decc10d28711c14b3229c0f4cec986d5596fc
kiwi0825 <nbeinniwllek@gmail.com>
2026-01-09 18:02:12 -0500

    Fix Bug

--- StartScreen.java
+++ StartScreen.java
@@ -22,15 +22,13 @@ public class StartScreen extends World
         GreenfootImage bg = new GreenfootImage("StartScreenImage.png");
         bg.scale(getWidth(), getHeight());
         setBackground(bg);
-        
-        prepare();
-        
+                
     }
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    private void prepare()
+    public void act()
     {
         if (Greenfoot.isKeyDown("space")) {
             Greenfoot.setWorld(new MyWorld());
